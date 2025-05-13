cb47f50e16902e5ad0441dedc7e0760832854833
dlawjdals <yjm91804640@gmail.com>
2025-05-14 01:37:12 +0900

    식당 혼잡도 프로그램 제작.

---
+++ midterm_test/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //청주대에서 내가 불편한 것들.....
+        String name;
+        int people;
+
+    static class restaurant {
+        restaurant(String name) {
+            this.name = name;
+            this.crowdLevel = 0;
+        }
+    }
+
+
+
+
+
+
+    }
+}
