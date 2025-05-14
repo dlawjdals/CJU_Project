bc31539e88ba1de61b2cca55b4a0f84b8717ab6f
dlawjdals <yjm91804640@gmail.com>
2025-05-15 00:14:06 +0900

    식당 혼잡도 프로그램 제작

--- midterm_test/src/Main.java
+++ midterm_test/src/Main.java
@@ -3,13 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //청주대에서 내가 불편한 것들.....
+
+
+    static class restaurant {
         String name;
         int people;
 
-    static class restaurant {
         restaurant(String name) {
             this.name = name;
             this.crowdLevel = 0;
+        }
+
+        String getCrowdLevelText() {
+            return switch (crowdLevel) {
+                case 0 -> "여유";
+                case 1 -> "보통";
+                case 2 -> "혼잡";
+                default -> "정보 없음";
+            };
+
+
         }
     }
 
