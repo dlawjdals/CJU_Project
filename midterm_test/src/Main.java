4368d96f6e3501dd058817c86e0f38300f46f5bb
dlawjdals <yjm91804640@gmail.com>
2025-06-13 14:44:29 +0900

    프로그램 수정

--- midterm_test/src/Main.java
+++ midterm_test/src/Main.java
@@ -13,8 +13,6 @@ class restaurant {
         this.people = people;
     }
 
-    public restaurant(String name) {
-    }
 
     String getCrowdLevelText() {
         double ratio = (double) people / max_people;
@@ -43,17 +41,16 @@ class restaurant {
     }
 }
 public class Main {
-
     static ArrayList<restaurant> restaurants = new ArrayList<>();
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        restaurants.add(new restaurant("아르떼 카페", 10, 13));
-        restaurants.add(new restaurant("켐토 카페", 15, 5));
-        restaurants.add(new restaurant("중앙도서관 카페", 20, 21));
-        restaurants.add(new restaurant("학생회관 식당", 70, 38));
-        restaurants.add(new restaurant("비즈니스대 식당", 50, 49));
-        restaurants.add(new restaurant("짬봉의 맛", 30, 5));
+        restaurants.add(new restaurant("1. 아르떼 카페", 10, 13));
+        restaurants.add(new restaurant("2. 켐토 카페", 15, 5));
+        restaurants.add(new restaurant("3. 중앙도서관 카페", 20, 21));
+        restaurants.add(new restaurant("4. 학생회관 식당", 70, 38));
+        restaurants.add(new restaurant("5. 비즈니스대 식당", 50, 49));
+        restaurants.add(new restaurant("6. 짬뽕의 맛", 30, 5));
 
         while (true) {
             System.out.println("\n--- 청주대 캠퍼스 식당 혼잡도 시스템---");
@@ -61,7 +58,7 @@ public class Main {
             System.out.println("2. 인원 수 수동 등록");
             System.out.println("3. 시간 경과 시뮬레이션");
             System.out.println("4. 종료");
-            System.out.println("메뉴 선택");
+            System.out.print("메뉴 선택: ");
 
             int choice = scanner.nextInt();
 
@@ -87,7 +84,8 @@ public class Main {
 
     static void registerCrowdStatus() {
         showCrowdStatus();
-        System.out.println("혼잡도를 등록할 식당 번호 선택 > ");
+        System.out.print("\n인원을 등록할 장소 번호 입력 (1 ~ " + restaurants.size() + ") > ");
+
         int choice = scanner.nextInt() - 1;
 
         if (choice < 0 || choice >= restaurants.size()) {
@@ -95,32 +93,36 @@ public class Main {
             return;
         }
 
-        System.out.println("혼잡도 입력 (0: 여유, 1: 보통, 2: 혼잡)");
-        int level = scanner.nextInt();
+        System.out.println("추가할 인원 수 입력 > ");
+        int add = scanner.nextInt();
 
-        if (level < 0 || level > 2) {
-            System.out.println("잘못된 혼잡도입니다.");
+        if (add < 0) {
+            System.out.println("잘못된 인원 수 입니다.");
             return;
 
         }
 
-        restaurants.get(level).addPeople(level);
-        System.out.println("혼잡도 등록이 완료되었습니다!");
-
-        static void simulateFuture() {
-            System.out.println("\n 40분 후 혼잡도 업데이트 중...");
-
-            restaurants.get(0).updatePeople(1);
-            restaurants.get(1).updatePeople(14);
-            restaurants.get(2).updatePeople(10);
-            restaurants.get(3).updatePeople(15);
-            restaurants.get(4).updatePeople(10);
-            restaurants.get(5).updatePeople(75);
-
-            showCrowdStatus();
-
-        }
+        restaurants.get(choice).addPeople(add);
+        System.out.println("인원 수 등록이 완료되었습니다!");
     }
+
+    static void simulateFuture() {
+        showCrowdStatus();
+        System.out.println("\n 40분 후 혼잡도 업데이트 중...");
+
+        restaurants.get(0).updatePeople(1);
+        restaurants.get(1).updatePeople(14);
+        restaurants.get(2).updatePeople(10);
+        restaurants.get(3).updatePeople(15);
+        restaurants.get(4).updatePeople(10);
+        restaurants.get(5).updatePeople(75);
+
+        showCrowdStatus();
+
+    }
+
+
+
 
 
 
